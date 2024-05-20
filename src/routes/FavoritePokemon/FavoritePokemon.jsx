102f8d8092705e53c001dd041a8d6919e7210550
destradax <destradax@gmail.com>
2024-05-19 20:46:31 -0500

    ads grid and styles to the favorites page

--- src/routes/FavoritePokemon/FavoritePokemon.jsx
+++ src/routes/FavoritePokemon/FavoritePokemon.jsx
@@ -1,66 +1,27 @@
-import { getMultiplePokemonById } from 'api/pokemon';
 import clsx from 'clsx';
 import { useFavorites } from 'context/favoritePokemon';
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './FavoritePokemon.module.scss';
+import PokemonCard from './PokemonCard';
 
 const FavoritePokemon = () => {
   const navigate = useNavigate();
 
-  const { favorites, addFavorite, removeFavorite } = useFavorites();
+  const { favorites } = useFavorites();
 
-  const [pokemon, setPokemon] = useState([]);
-
-  useEffect(() => {
-    const refreshFavoritesInfo = async () => {
-      setPokemon(await getMultiplePokemonById(favorites));
-    };
-
-    refreshFavoritesInfo();
-  }, [favorites]);
-
-  const handleSelectPokemon = p => navigate(`/pokemon/${p.id}`);
-
-  const handleToggleFavorite = (pokemonId, isFavorite) => {
-    if (isFavorite) {
-      addFavorite(pokemonId);
-    } else {
-      removeFavorite(pokemonId);
-    }
-  };
+  const handleSelectPokemon = pokemonId => navigate(`/pokemon/${pokemonId}`);
 
   return (
     <div className={clsx(styles.favoritePokemon)}>
       <h1>Favorite Pokemon</h1>
-
-      <div className={styles.list}>
-        <div className={clsx(styles.listItem, styles.listHeader)}>
-          <div>Pokemon Name</div>
-          <div>Favorite?</div>
-        </div>
-
-        {pokemon.map(p => {
-          const isFavorite = favorites.includes(p.id);
-
-          return (
-            <div
-              key={p.name}
-              className={clsx(styles.listItem, styles.clickableListItem)}
-              onClick={() => handleSelectPokemon(p)}
-            >
-              <div className={styles.pokemonName}>{p.name}</div>
-              <button
-                onClick={e => {
-                  e.stopPropagation();
-                  handleToggleFavorite(p.id, !isFavorite);
-                }}
-              >
-                {isFavorite ? 'Yes' : 'No'}
-              </button>
-            </div>
-          );
-        })}
+      <div className={styles.grid}>
+        {favorites.map(id => (
+          <PokemonCard
+            key={id}
+            pokemonId={id}
+            onClick={() => handleSelectPokemon(id)}
+          />
+        ))}
       </div>
     </div>
   );
---
+++ src/routes/FavoritePokemon/PokemonCard/PokemonCard.jsx
@@ -0,0 +1,37 @@
+import { getPokemonDetails } from 'api/pokemon';
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { getPokemonArtworkUrl } from 'routes/Home/PokemonList/PokemonListItem/PokemonListItem.service';
+import styles from './PokemonCard.module.scss';
+
+const PokemonCard = ({ pokemonId, onClick }) => {
+  const [pokemonData, setPokemonData] = useState(null);
+
+  useEffect(() => {
+    const refreshInfo = async () => {
+      setPokemonData(await getPokemonDetails(pokemonId));
+    };
+
+    refreshInfo();
+  }, [pokemonId]);
+
+  return (
+    <div className={styles.pokemonCard} onClick={onClick}>
+      <img
+        src={getPokemonArtworkUrl(pokemonData)}
+        alt="artwork"
+        className={styles.artwork}
+      />
+      <div className={styles.pokemonInfo}>
+        <span className={styles.pokemonName}>{pokemonData?.name}</span>
+      </div>
+    </div>
+  );
+};
+
+PokemonCard.propTypes = {
+  pokemonId: PropTypes.number.isRequired,
+  onClick: PropTypes.func
+};
+
+export default PokemonCard;
